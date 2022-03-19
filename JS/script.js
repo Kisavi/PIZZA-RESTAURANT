16c472374cf8abc93273462fcaa115be2bb98672
Kisavi <deniskisavi@gmail.com>
2022-03-20 02:40:32 +0300

    Ft - add function to show order summary on checkout

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,17 @@
 // USER INTERFACE
+// allow user to pick multiple toppings
 $(function () {
     $(".chzn-select").chosen();
 });
 
+// show user their order summary
+$(document).ready(function () {
+    $("#orderButton").click(function () {
+        $("#orderSummary").slideDown(1000);
+    });
+});
+
+// alert a message to user on feedback form submission
+function alertMessage() {
+    alert("We have received your message")
+}
